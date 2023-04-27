f7f5d369b1cfe37e945ec54e1db842afb52082ab
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-27 12:23:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,44 +3,46 @@ Hello {mention}
 
 I'm an advanced filter bot with many capabilities!
 
-There is no practical limits for my filtering capacity :)
+There is no practical limit for my filtering capacity :)
 
-See help button for commands and more details
+See the help button for commands and more details
 """
 
 HelpTxT = """
-Add me as admin in your group and start filtering :)
+Add me as an admin in your group and start filtering :)
 
-<b>Basic Commands;</b>
+<b>Basic Commands:</b>
 
 /start - Check if I'm alive!
 /help - Command help
 /about - Something about me!
 
-<b>Filter Commands;</b>
+<b>Filter Commands:</b>
 
-<code>/add name reply</code>  -  Add filter for name
-<code>/del name</code>  -  Delete filter
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-<code>/viewfilters</code>  -  List all filters in chat
+<code>/add name reply</code> - Add filter for name
+<code>/del name</code> - Delete filter
+<code>/delall</code> - Delete all filters (Group Owner Only!)
+<code>/viewfilters</code> - List all filters in chat
 
-<b>Connection Commands;</b>
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+<b>Connection Commands:</b>
+<code>/connect groupid</code> - Connect your group to my PM. You can also simply use
 <code>/connect</code> in groups.
-<code>/connections</code>  -  Manage your connections.
+<code>/connections</code> - Manage your connections.
 
-<b>Extras;</b>
-/status  -  Shows current status of your bot (Auth User Only)
-/id  -  Shows ID information
-/info userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+<b>Extras:</b>
+/status - Show the current status of your bot (Auth User Only)
+/id - Show ID information
+/info userid - Show user information. Use <code>/info</code> as a reply to some message for their details!
 
 © @Mo_Tech_YT
 """
 
 AboutTxT = """
-⭕️ My Name : TroJanz Filter Bot</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
-⭕️ Language : Python3
-⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
-"""
+<b>TroJanz Filter Bot</b>
+⭕️ Developers: @TroJanzHEX 
+⭕️ Upgrader: @Mo_Tech_YT
+⭕️ Language: Python3
+⭕️ Library: <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a>
+"""```
+
+I have fixed the spelling errors and formatting issues in the strings.
