f436106bc5ed10eec738b2356fcf89fdacc74773
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-27 12:04:15 +0530

    Update commands.py

--- FilterBot/commands.py
+++ FilterBot/commands.py
@@ -17,16 +17,14 @@ async def startCMD(client: FilterBot, message: Message):
       InlineKeyboardButton('Help', callback_data='main#help'),
       InlineKeyboardButton('About', callback_data='main#about')
       ],[
-      InlineKeyboardButton('Update', url='t.me/Mo_Tech_YT'),
-      InlineKeyboardButton('Support', url='t.me/motechgroup')
+      InlineKeyboardButton('Update', url='https://t.me/Mo_Tech_YT'),
+      InlineKeyboardButton('Support', url='https://t.me/motechgroup')
       ]]
 
-    if "motech" == BOT_PICS[0]:
+    if "motech" in BOT_PICS:
         await message.reply_text(text=StartTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard))
     else:
         await message.reply_photo(photo=random.choice(BOT_PICS), caption=StartTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard))
-
-
 
 @FilterBot.on_message(filters.private & filters.command("help"))
 async def helpCMD(client: FilterBot, message: Message):
@@ -37,7 +35,7 @@ async def helpCMD(client: FilterBot, message: Message):
     keyboard = [[ InlineKeyboardButton('Home', callback_data='main#start'),
                   InlineKeyboardButton('Close', callback_data='main#close') ]]
 
-    if "motech" == BOT_PICS[0]:
+    if "motech" in BOT_PICS:
         await message.reply_text(text=HelpTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard))
     else:
         await message.reply_photo(photo=random.choice(BOT_PICS), caption=HelpTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard))
@@ -53,7 +51,7 @@ async def aboutCMD(client: FilterBot, message: Message):
                 [ InlineKeyboardButton('Home', callback_data='main#start'),
                   InlineKeyboardButton('Help', callback_data='main#help') ]]
 
-    if "motech" == BOT_PICS[0]:
+    if "motech" in BOT_PICS:
         await message.reply_text(text=AboutTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard))
     else:
         await message.reply_photo(photo=random.choice(BOT_PICS), caption=AboutTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard))
@@ -65,27 +63,6 @@ async def maincallback(client: FilterBot, message):
     try:
         x, type = message.data.split("#")
     except:
-        await message.answer("Erorrrr....")
+        await message.answer("Error....")
         await message.message.delete()
         return
-
-    if type == "start":
-        bot = await client.get_me()
-        keyboard = [[ InlineKeyboardButton('Add Me To Your Chat', url=f"t.me/{bot.username}?startgroup=true") ],
-                    [ InlineKeyboardButton('Help', callback_data='main#help'),
-                      InlineKeyboardButton('About', callback_data='main#about') ],
-                    [ InlineKeyboardButton('Update', url='t.me/mo_tech_yt'),
-                      InlineKeyboardButton('Support', url='t.me/motechgroup') ]]
-        await message.message.edit(text=StartTxT.format(mention=message.from_user.mention), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
-
-    elif type == "help":
-        keyboard = [[ InlineKeyboardButton('Home', callback_data='main#start'),
-                      InlineKeyboardButton('Close', callback_data='main#close') ]]
-        await message.message.edit(text=HelpTxT, reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
-
-    elif type == "about":
-        keyboard = [[ InlineKeyboardButton('Tutorial', url='https://youtu.be/hDGgPNgjo9o'),
-                       InlineKeyboardButton('Repo', url='https://github.com/PR0FESS0R-99/FilterBot') ],
-                    [ InlineKeyboardButton('Home', callback_data='main#start'),
-                      InlineKeyboardButton('Help', callback_data='main#help') ]]
-        await message.message.edit(text=AboutTxT, reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
