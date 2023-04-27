8cbbda637eca308e82fadec87dd612aa355f8d45
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-27 12:07:35 +0530

    Update connections.py

--- FilterBot/connections.py
+++ FilterBot/connections.py
@@ -1,7 +1,8 @@
-import logging, pyrogram 
+import logging
+import pyrogram 
 from pyrogram import filters, Client, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from FilterBot.database import db
+from FilterBot import database as db
 from configs import ADMINS
 
 logger = logging.getLogger(__name__)
@@ -86,60 +87,4 @@ async def deleteconnection(client, message):
     chat_type = message.chat.type
 
     if chat_type == enums.ChatType.PRIVATE:
-        await message.reply_text("Run /connections to view or disconnect from groups!", quote=True)
-
-    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        group_id = message.chat.id
-
-        st = await client.get_chat_member(group_id, userid)
-        if (
-                st.status != enums.ChatMemberStatus.ADMINISTRATOR
-                and st.status != enums.ChatMemberStatus.OWNER
-                and str(userid) not in ADMINS
-        ):
-            return
-
-        delcon = await db.delete_connection(str(userid), str(group_id))
-        if delcon:
-            await message.reply_text("Successfully disconnected from this chat", quote=True)
-        else:
-            await message.reply_text("This chat isn't connected to me!\nDo /connect to connect.", quote=True)
-
-@Client.on_message(filters.private & filters.command(["connections"]))
-async def connections(client, message):
-    userid = message.from_user.id
-
-    groupids = await db.all_connections(str(userid))
-    if groupids is None:
-        await message.reply_text(
-            "There are no active connections!! Connect to some groups first.",
-            quote=True
-        )
-        return
-    buttons = []
-    for groupid in groupids:
-        try:
-            ttl = await client.get_chat(int(groupid))
-            title = ttl.title
-            active = await db.if_active(str(userid), str(groupid))
-            act = " - ACTIVE" if active else ""
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{act}"
-                    )
-                ]
-            )
-        except:
-            pass
-    if buttons:
-        await message.reply_text(
-            "Your connected group details ;\n\n",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            quote=True
-        )
-    else:
-        await message.reply_text(
-            "There are no active connections!! Connect to some groups first.",
-            quote=True
-        )
+        await message.reply_text("Run /connections to view or disconnect from groups!", quote=True
